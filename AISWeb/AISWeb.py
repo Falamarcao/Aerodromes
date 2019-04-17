ca3a77bf1f17cb8a8df5bfecc56e4d18a0ac6dc6
Falamarcao <falamarcao@gmail.com>
2019-04-17 17:47:32 -0300

    Notam, Alerts updated

--- AISWeb/AISWeb.py
+++ AISWeb/AISWeb.py
@@ -3,7 +3,6 @@ from multiprocessing import Pool, current_process
 from lxml.html import fromstring
 from bs4 import BeautifulSoup
 from csv import DictWriter
-from re import compile
 from os import listdir
 import hashlib
 import time
@@ -18,7 +17,10 @@ class AISWeb(object):
         self.response_exception = None
         self.bs = None
         self.results: list = []
-        self.headers: dict = {"User-Agent": "Chrome/62.0.3202.94 Safari/537.36"}
+        self.headers: dict = {"Accept": "text/html,application/xhtml+xml,application/xml;"
+                                        "q=0.9,image/webp,image/apng,*/*;"
+                                        "q=0.8,application/signed-exchange;v=b3",
+                              "User-Agent": "Studentbot"}
 
     def get(self, name: str, url: str, params: dict = None):
         """
@@ -33,20 +35,46 @@ class AISWeb(object):
         except Exception as e:
             self.response_exception = e
             print("-" * 100)
-            print(f" {e} happened on {name} with param list:\n{params}\n")
+            print(f"|HTTP GET| {e} happened on {name} with params:\n{params}\n")
             print("-" * 100, "\n")
             return False
 
-        if response.status_code == 200 or 404:
+        if response.status_code == 200:
             return response
         else:
             print("-" * 100)
-            print(f"Response code: {response.status_code} on {name} with param list:\n{params}")
+            print(f"|HTTP GET| Response code: {response.status_code} on {name} with params:\n{params}")
+            print("-" * 100, "\n")
+            return {}
+
+    def post(self, name: str, url: str, data: dict = None, params: dict = None):
+        """
+        function to get json data from API
+        :param name: transaction name for log control
+        :param url: from where are asking data
+        :param data: web form data
+        :param params: filters and mandatory parameters
+        :return: Json or False when status_code isn't 200.
+        """
+        try:
+            response = self.Session.post(url, data=data, params=params, headers=self.headers)
+        except Exception as e:
+            self.response_exception = e
+            print("-" * 100)
+            print(f"|HTTP POST| {e} happened on {name} with data: {data} and params:\n{params}\n")
+            print("-" * 100, "\n")
+            return False
+
+        if response.status_code == 200:
+            return response
+        else:
+            print("-" * 100)
+            print(f"|HTTP POST| Response code: {response.status_code} on {name} with data: {data} and params:{params}")
             print("-" * 100, "\n")
             return {}
 
     # extract values
-    def get_icao(self, icao=None):
+    def get_icao(self, icao: str = None):
         try:
             return self.bs.find("strong", {"title": "Indicador de Localidade (ICAO Code)"}).text
         except AttributeError:
@@ -59,13 +87,12 @@ class AISWeb(object):
             return element.text
         return ""
 
-    @property
-    def get_alert(self):
+    def get_alert(self, icao: str):
         page = fromstring(self.response.content)
         try:
-            s = page.xpath('/html/body/div/div/div/div[1]/div/div/strong/text()')[0] + \
+            alert = page.xpath('/html/body/div/div/div/div[1]/div/div/strong/text()')[0] + \
                 page.xpath('/html/body/div/div/div/div[1]/div/div/text()')[1]
-            return s.strip()
+            return alert.strip()
         except IndexError:
             element = page.xpath('/html/body/div/div/section/div/div[1]/div/div/text()')
             if element is not None:
@@ -75,45 +102,34 @@ class AISWeb(object):
                         value += i.strip() + ' '
                     else:
                         value += i.strip()
-                return value
-        return ""
+            if value.strip() == "O aeródromo não foi encontrado.":
+                # search fro details
+                params: dict = {"i": "busca"}
+                data: dict = {"q": icao}
+                response = self.post(name="alert", url="https://www.aisweb.decea.gov.br/", params=params, data=data)
+                alert = fromstring(response.content).xpath('/html/body/div/div/section[1]/div/div/div[2]'
+                                                           '/div/div/div[2]/div/div/strong/text()')[0]
+                del response, data, params
+                return alert.strip()
+            return "OK"
 
     @property
     def get_notam(self):
         elements = self.bs.find_all("div", {"class": "notam"})
-
         if elements is not None:
-            notam_list = []
+            lstnotam = []
+            strnotam = ""
             for element in elements:
-                notam_dict = {}
-                badge_info = element.find_next('span', {'class': compile('.*badge.*')})
-                if badge_info is not None:
-                    notam_dict.update({"badge_info": badge_info.text})
-
-                    h5 = element.find_next('h5')
-                    if h5 is not None:
-                        notam_dict.update({"titulo": h5.text[len(badge_info.text)+1:]})
-
-                    msg = element.find_next('div', {'class': 'msg'})
-                    if msg is not None:
-                        notam_dict.update({"msg": msg.text})
-
-                    pre = element.find_next('pre')
-                    if pre is not None:
-                        notam_dict.update({"texto1": pre.text})
-
-                    span = element.find_next('span', {'class': ''})
-                    if badge_info is not None:
-                        notam_dict.update({"texto2": span.text})
-
-                    href = element.find_next('a').get('href')
-                    if (href is not None) and ("?i=aerodromos&p=sol&id=" not in href):
-                        notam_dict.update({"anexo": href})
-                notam_list.append(notam_dict)
-
-            if len(notam_list) == 1:
-                return notam_list
-            return notam_list
+                element = str(element).split('<')
+                for x in range(1, len(element)-1):
+                    line = element[x].split('>')[1]
+                    if (len(line) > 0) and (line[-1] != " "):  # add space where don't have spaces
+                        line += " "
+                    strnotam += line
+                lstnotam.append(strnotam.strip())
+            if len(lstnotam) == 1:
+                return strnotam
+            return lstnotam
         return ""
 
     # search
@@ -129,12 +145,11 @@ class AISWeb(object):
                        "Aeródromo": self.get_value("span", "Nome do Aeródromo"),
                        "Cidade": self.get_value("span", "cidade"),
                        "UF": self.get_value("span", "Estado"),
-                       "Alerta": self.get_alert,
+                       "Status": self.get_alert(icao),
                        "NOTAM": self.get_notam,
-                       "STATUS": "OK"}
+                       "Debug": {"response_status_code": self.response.status_code}}
             return results
-        return {"ICAO": icao, "STATUS": {"status code": self.response.status_code,
-                                         "Exception": self.response_exception}}
+        return {"ICAO": icao, "DEBUG": {"Exception": self.response_exception}}
 
     @property
     def read_icao_file(self) -> list:
@@ -179,5 +194,6 @@ class AISWeb(object):
 
 if __name__ == '__main__':
     aisweb = AISWeb()
-    aisweb.search_by_list_of_icao()
-    aisweb.to_csv()
+    aisweb.search_by_list_of_icao(['SWDM'])
+    print(aisweb.results)
+    # aisweb.to_csv()
